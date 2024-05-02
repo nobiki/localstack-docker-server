d49edc551aaa1286dbb4fa42d898f28aa35f0766
Naoaki Obiki <obiki.wk@gmail.com>
2024-05-02 19:26:09 +0900

    add IaC testcode

---
+++ iac/tofu/main.tf
@@ -0,0 +1,24 @@
+## test code
+
+terraform {
+  backend "local" {}
+}
+
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = "dummy"
+  secret_key = "dummy"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://localhost:4566" # LocalStack Gateway
+  }
+}
+
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "my-tf-test-bucket"
+}
